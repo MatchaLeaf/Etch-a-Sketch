cf3f61c484a16f963c79a9646ad1c0a2eb014681
Adrian <matchadev22@gmail.com>
2025-09-29 22:26:46 +1300

    Adds Opacity and updates based on slider value

--- EAS.js
+++ EAS.js
@@ -1,12 +1,40 @@
 const container = document.querySelector("#container");
+const colorPicker = document.querySelector("#colorInput");
+const opacitySlider = document.querySelector("#opacity");
+const opacityLabel = document.querySelector("#opacityLabel");
+const selectedColor = colorPicker.value;
+
 let isHolding = false;
-let defaultColor = 'black';
+let defaultColor = '#000000';
 let mode = "normal";
+let defaultOpacity = opacitySlider.value;
 
 //create 3 event types that will "listen" to what the user inputs
 container.addEventListener("mousedown", () =>{ isHolding =  true; });
 container.addEventListener("mouseup", () => { isHolding = false; });
 container.addEventListener("mouseleave", () =>{ isHolding = false; })
+
+
+//listen for the color change taking place
+colorPicker.addEventListener("change", ()  => {
+    defaultColor = colorPicker.value;
+})
+
+//listen for the change with the slider
+opacity.addEventListener("input", () => {
+    const value = opacitySlider.value;
+    opacityLabel.textContent = value;
+    defaultOpacity = opacitySlider.value;
+}
+)
+
+function hexToRgb(hex){
+    hex = hex.replace("#", "");
+    const r = parseInt(hex.substring(0,2), 16);
+    const g = parseInt(hex.substring(2,4), 16);
+    const b = parseInt(hex.substring(4,6), 16);
+    return `${r}, ${g}, ${b}`;
+}
 
 function Normal(){
 
@@ -16,29 +44,35 @@ while (container.firstChild){
 }
 
  for(let i = 1; i <= 256; i++){
-    mode = "normal";
-    defaultColor = "black";
     const div = document.createElement("div");
+    div.classList.add("grid-cell");
     container.appendChild(div);
 
 
     div.addEventListener("mousemove", () => {
         setTimeout(()=>{
+            const opacity = opacitySlider.value / 100;
             if(isHolding){ 
                 if(mode == "normal"){
-                div.style.backgroundColor = defaultColor;
+                  const rgb = hexToRgb(defaultColor);
+                  div.style.backgroundColor = `rgba(${rgb}, ${opacity})`
                 }else if(mode == "random"){
-                div.style.backgroundColor = RandomColors();
+                    const rgb = RandomColors();
+                    div.style.backgroundColor = `rgba(${rgb}, ${opacity})`;
                 }
             }
         }, 50); 
     });
 
     div.addEventListener("mousedown", () => {
+        const opacity = opacitySlider.value / 100;
         if(mode == "normal"){
-            div.style.backgroundColor = defaultColor;
+            const rgb = hexToRgb(defaultColor);
+            div.style.backgroundColor = `rgba(${rgb}, ${opacity})`
         }else if(mode == "random"){
-            div.style.backgroundColor = RandomColors();
+            const rgb = RandomColors();
+            div.style.backgroundColor = `rgba(${rgb}, ${opacity})`;
+
         }
     })
 }
@@ -50,33 +84,35 @@ function RandomColors() {
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
     
-    return `rgb(${r}, ${g}, ${b})`; //Returns RBG as a string 
+    return `${r}, ${g}, ${b}`; //Returns RBG as a string 
 }
 
 function Eraser(){
     mode = "normal";
-    defaultColor = "white";
+    defaultColor = "#FFFFFF";
 }
 
 function Random(){
     mode = "random";
-    console.log(mode);
 }
 
 function AskUser(){
     //To get what the user inputted inside the textbox we use the .value tag
     const input = document.querySelector("#gridSizeInput");
-    const n = parseInt(input.value);
+    const n = Number(input.value);
 
-    if(n === null){
+    if(isNaN(n)){
         alert("Please Enter a number from 8 to 100");
         input.value = "";
+        return;
     }else if(n < 8){
         alert("This number is too small (pick between 8 to 100)")
         input.value = "";
+        return;
     }else if(n > 100){
         alert("Sorry this number is too big (Please Enter a number between 8 to 100");
         input.value = "";
+        return;
     }else if(n <= 100){
         input.value = "";
         //First clear existing grid
@@ -87,6 +123,7 @@ function AskUser(){
         //Set the grid size to the designated value
         for(let i = 1; i <= n*n; i++){
             const newDiv = document.createElement("div");
+            newDiv.classList.add("grid-cell");
             container.appendChild(newDiv);
 
             newDiv.style.height = (100 / n) + "%";
@@ -102,9 +139,13 @@ function AskUser(){
                 //start drawing
                 if(isHolding){
                     if(mode === "normal"){
-                    newDiv.style.backgroundColor = defaultColor;
-                    } else if(mode ==- "random"){
-                    newDiv.style.backgroundColor = RandomColors();
+                        const rgb = hexToRgb(defaultColor);
+                        const opacity = opacitySlider.value / 100;
+                        newDiv.style.backgroundColor = `rgba(${rgb}, ${opacity})`
+                    } else if(mode == "random"){
+                        const rgb = RandomColors();
+                        const opacity = opacitySlider.value / 100;
+                        newDiv.style.backgroundColor = `rgba(${rgb}, ${opacity})`;
                     }
                 }     
             });
@@ -116,9 +157,13 @@ function AskUser(){
                 setTimeout (() => {
                     if(isHolding){ 
                         if(mode === "normal") {
-                            newDiv.style.backgroundColor = defaultColor;
+                            const rgb = hexToRgb(defaultColor);
+                            const opacity = opacitySlider.value / 100;
+                            newDiv.style.backgroundColor = `rgba(${rgb}, ${opacity})`
                         }else if(mode === "random"){
-                            newDiv.style.backgroundColor = RandomColors();
+                            const rgb = RandomColors();
+                            const opacity = opacitySlider.value / 100;
+                            newDiv.style.backgroundColor = `rgba(${rgb}, ${opacity})`;
                         }
                     }
                 }, 100);
@@ -128,18 +173,19 @@ function AskUser(){
     
     }else if(!Number.isInteger(n)){
         alert("Please Enter a number");
+        return;
     }
 }
 
 function Clear(){
     //first select all of the divs on the screens
-   const div = document.querySelectorAll("div");
+   const gridCell = document.querySelectorAll(".grid-cell");
    //For each of those divs set the background color to white
-   div.forEach( div => div.style.backgroundColor = "white");
+   gridCell.forEach( cells => cells.style.backgroundColor = "white");
 }
 
 function Pencil(){
     mode = "normal";
-    defaultColor = "black";
+    defaultColor = colorPicker.value;
 }
 Normal();
